70e11cff9f5e4c8b1e8463da2ceb21e2f6a89306
gsery <gsery@TABLET-OQ2E6VNT>
2017-02-01 07:25:02 -0600

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,20 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing spider man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue ! Witness the final hours of doctor Octopus'life and his great act of revenge! Even if Spider-Man survies... WILL PETER PARKER?</p>";
+            ViewBag.Artists = new string[]
+        {
+            "Script: Dan Slott",
+            "Pencils: Humberto Ramos",
+            "Inks: victor Olazaba",
+            "Colors: Edgar Delgado",
+            "letters: Chris Eliopoulos"
+        };
+
+
+
             return View();
           
 
